3dec41a6afb0b1e87f8f468bb696d45511bcc5a8
Anubhav Rawat <anubhav2021rawat@gmail.com>
2026-06-09 04:12:41 +0530

    Added Customer Class

--- CompanyOrder.java
+++ CompanyOrder.java
@@ -2,13 +2,28 @@ import java.util.ArrayList;
 
 class company {
     private String name;
+    // private String Items;
+    private ArrayList<Item> items;
 
     public company(String name) {
         this.name = name;
+        this.items = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public ArrayList<Item> getItems() {
+        return (ArrayList<Item>) this.items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
     }
 
     public void setName(String name) {
@@ -19,17 +34,58 @@ class company {
 
 class Customer {
     private String name;
-
-    public Customer(String name) {
+    private String id;
+    public Customer(String name,String id) {
         this.name = name;
+        this.id=id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setId() {
+        this.id = id;
+    }
+}
+
+class Item {
+    private int itemNumber;
+    private String title;
+    private int rate;
+
+    public Item(int itemNumber, String title, int rate) {
+        this.itemNumber = itemNumber;
+        this.title = title;
+        this.rate = rate;
+    }
+
+    public int getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(int itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
     }
 }
 
